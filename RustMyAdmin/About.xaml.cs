dee39a38560317e23adf6e0403a974394e5a3ea4
Simon Cahill <contact@simonc.eu>
2022-12-30 02:09:09 +0100

    Began playing around with the MAUI tutorials. Seems fun

---
+++ RustMyAdmin/About.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace RustMyAdmin;
+
+public partial class About : ContentPage {
+
+	private int TimesClicked { get; set; } = 0;
+
+	public About() {
+		InitializeComponent();
+	}
+
+	void OnButtonClicked(System.Object sender, System.EventArgs e) {
+		((Button)sender).Text = String.Format("Clicked {0} time{1}", ++TimesClicked, (TimesClicked) == 1 ? "" : "s");
+	}
+}
