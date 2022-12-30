b341eccd601314cbe5baa088345c1ce79f645bad
Simon Cahill <contact@simonc.eu>
2022-12-30 19:19:47 +0100

    Added a quick and dirty test button

--- RustMyAdmin/About.xaml.cs
+++ RustMyAdmin/About.xaml.cs
@@ -6,4 +6,10 @@ public partial class About : ContentPage {
 	public About() {
 		InitializeComponent();
 	}
+
+	int timesClicked = 0;
+
+	void Button_Clicked(System.Object sender, System.EventArgs e) {
+		((Button)sender).Text = $"You clicked { ++timesClicked } time{ (timesClicked == 1 ? "" : "s") }";
+	}
 }
