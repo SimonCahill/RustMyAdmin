60fa67a5aba54776c5d493b30cb157aca4d8243a
Simon Cahill <contact@simonc.eu>
2022-12-31 16:21:29 +0100

    Began preparing for loading languages

--- RustMyAdmin/App.xaml.cs
+++ RustMyAdmin/App.xaml.cs
@@ -1,12 +1,19 @@
 ﻿namespace RustMyAdmin;
 
-public partial class App : Application
-{
-	public App()
-	{
+public partial class App : Application {
+
+	const string TranslationDirPath = "Resources/Translations/";
+	const string LanguageFileExt	= ".lang";
+
+	internal List<FileInfo> LanguageFiles { get; private set; } = default;
+
+	public App() {
 		InitializeComponent();
 
 		MainPage = new AppShell();
 	}
+
+
+	
 }
 
