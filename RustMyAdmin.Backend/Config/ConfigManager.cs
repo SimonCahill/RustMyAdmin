63a56fb6fbf537103e983f2fefe5c23793d6839f
Simon Cahill <contact@simonc.eu>
2023-01-02 22:31:55 +0100

    Finished implementation of ConfigManager

--- RustMyAdmin.Backend/Config/ConfigManager.cs
+++ RustMyAdmin.Backend/Config/ConfigManager.cs
@@ -11,16 +11,26 @@ namespace RustMyAdmin.Backend.Config {
 
         public Dictionary<string, object> Configurations { get; private set; }
 
+        private FileInfo m_configFile;
+
         readonly Regex MultiConfRegex = new Regex(@"([A-z_]\.?)+");
 
-        public ConfigManager() {
+        public ConfigManager(FileInfo? configFile = null) {
             Configurations = new Dictionary<string, object>();
+            m_configFile = configFile ?? Extensions.ConfigFile;
         }
 
         public async Task LoadConfigAsync() {
-            using (var fReader = Extensions.ConfigFile.OpenText()) {
+            using (var fReader = m_configFile.OpenText()) {
                 var fContents = await fReader.ReadToEndAsync();
                 Configurations = JsonConvert.DeserializeObject<Dictionary<string, object>>(fContents) ?? new Dictionary<string, object>();
+            }
+        }
+
+        public async Task SaveConfigAsync() {
+            using (var fWriter = new StreamWriter(m_configFile.OpenWrite())) {
+                var jsonString = JsonConvert.SerializeObject(Configurations);
+                await fWriter.WriteAsync(jsonString);
             }
         }
 
@@ -45,6 +55,8 @@ namespace RustMyAdmin.Backend.Config {
         /// <param name="config">The name of the config to check for.</param>
         /// <returns><code >true</code> if the config name was found.</returns>
         public bool HasConfig(string config) {
+            if (string.IsNullOrEmpty(config)) { throw new ArgumentNullException(nameof(config), "The name of the config must not be null or empty!"); }
+
             if (MultiConfRegex.IsMatch(config)) {
                 return HasConfig(Configurations, config);
             }
@@ -58,9 +70,70 @@ namespace RustMyAdmin.Backend.Config {
 
 
             if (MultiConfRegex.IsMatch(configName)) {
-                var indexOfFirstPeriod = configName.IndexOf('.');
+                var curSection = GetConfigSection(configName, out var nextSection);
+                if (!cfg.ContainsKey(curSection)) { return false; }
 
+                if (cfg.TryGetValue(configName, out var subConf)) {
+                    if (IsConfigDictSubType(subConf)) {
+                        return HasConfig(subConf as Dictionary<string, object>, nextSection);
+                    }
+                }
+
+                return false;
             }
+
+            return cfg.ContainsKey(configName);
+        }
+
+        /// <summary>
+        /// Gets the desired config with specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter to retrieve. This method will attempt to cast the config value to the type passed.</typeparam>
+        /// <param name="configName">The name of the configuration to retrieve.</param>
+        /// <returns>The config value.</returns>
+        /// <exception cref="ArgumentNullException">If the input parameter "configName" is null or empty.</exception>
+        /// <exception cref="ConfigNotFoundException">If the desired configuration could not be found.</exception>
+        public T GetConfig<T>(string configName) {
+            if (string.IsNullOrEmpty(configName)) { throw new ArgumentNullException(nameof(configName), "The name of the config must not be null or empty!"); }
+            if (!HasConfig(configName)) { throw new ConfigNotFoundException(configName); }
+
+            return GetConfig<T>(Configurations, configName);
+        }
+
+        private T GetConfig<T>(Dictionary<string, object> obj, string configName) {
+            if (string.IsNullOrEmpty(configName)) { throw new ArgumentNullException(nameof(configName), "The name of the config must not be null or empty!"); }
+            if (obj == null) { return default; }
+            if (!HasConfig(obj, configName)) { throw new ConfigNotFoundException(configName); }
+
+            var nestedConfig = MultiConfRegex.IsMatch(configName);
+
+            if (nestedConfig) {
+                var curSection = GetConfigSection(configName, out var nextSection);
+                if (!obj.TryGetValue(curSection, out var subconf)) {
+                    if (!IsConfigDictSubType(subconf)) { return (T)subconf; }
+
+                    return GetConfig<T>(subconf as Dictionary<string, object>, nextSection);
+                }
+            }
+
+            if (obj.TryGetValue(configName, out var conf)) {
+                return (T)conf;
+            }
+
+            throw new ConfigNotFoundException(nameof(configName));
+        }
+
+        private bool IsConfigDictSubType(object obj) => obj is Dictionary<string, object>;
+
+        private string GetConfigSection(string config, out string next) {
+            if (!MultiConfRegex.IsMatch(config)) {
+                next = string.Empty;
+                return config;
+            }
+
+            var indexOfFirstPeriod = config.IndexOf('.');
+            next = config.Substring(indexOfFirstPeriod + 1);
+            return config.Substring(0, indexOfFirstPeriod - 1);
         }
 
     }
