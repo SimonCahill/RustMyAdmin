1accab717a4b38aa4e84e943e9906aca3d8138e9
Simon Cahill <contact@simonc.eu>
2023-01-02 23:10:42 +0100

    Began implementing ViewModels

---
+++ RustMyAdmin/ViewModels/AboutViewModel.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * AboutViewModel.cs
+ *
+ * Author: Simon Cahill (simon@simonc.eu)
+ * File creation: 02.01.2023
+ *
+ * Copyright: © Simon Cahill - All Rights Reserved
+ *
+ */
+
+using System;
+
+namespace RustMyAdmin.ViewModels {
+
+    using Backend.Config;
+
+    public class AboutViewModel: BaseViewModel {
+
+        ConfigManager m_configManager;
+
+        public AboutViewModel() {
+            base.Instance = this;
+            m_configManager = ConfigManager.Instance;
+        }
+
+
+
+    }
+}
+
---
+++ RustMyAdmin/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * BaseViewModel.cs
+ *
+ * Author: Simon Cahill (simon@simonc.eu)
+ * File creation: 02.01.2023
+ *
+ * Copyright: © Simon Cahill - All Rights Reserved
+ *
+ */
+
+using System;
+
+namespace RustMyAdmin.ViewModels {
+
+    using System.ComponentModel;
+
+    public abstract class BaseViewModel: INotifyPropertyChanged {
+
+        protected BaseViewModel Instance { get; set; }
+
+        public BaseViewModel() { }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propName) => PropertyChanged?.Invoke(Instance ?? this, new PropertyChangedEventArgs(propName));
+	}
+}
+
