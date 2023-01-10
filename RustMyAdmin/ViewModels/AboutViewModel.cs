bb15c088b22afdf3ac0d5ca0b46be124c714ba64
SimonC <simon@simonc.eu>
2023-01-10 21:49:56 +0100

    Updated About page

--- RustMyAdmin/ViewModels/AboutViewModel.cs
+++ RustMyAdmin/ViewModels/AboutViewModel.cs
@@ -14,6 +14,7 @@ namespace RustMyAdmin.ViewModels {
 
     using Backend.Config;
     using Backend.Parsers;
+    using Microsoft.UI.Xaml.Media.Imaging;
 
     public class AboutViewModel: BaseViewModel {
 
@@ -23,15 +24,16 @@ namespace RustMyAdmin.ViewModels {
         public AboutViewModel() {
             base.Instance = this;
             m_configManager = ConfigManager.Instance;
-            //m_language = LanguageParser.GetLanguageById(m_configManager.GetConfig<string>("Language"));
+            m_language = LanguageParser.GetLanguageById(m_configManager.GetConfig<string>("Language"));
         }
 
 
         public string AboutText {
-            //get => m_language.GetTranslation("about_page", "about_text");
-            get =>
-                "RustMyAdmin is the easy-to-use and all-encompassing Rust or Oxide server management system.\n" +
-                "";
+            get => m_language.GetTranslation("about_page", "about_text");
+        }
+
+        public string Copyright {
+            get => m_language.GetTranslation("about_page", "copyright");
         }
     }
 }
